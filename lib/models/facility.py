c700bb53048eeb8c3aa5e8c0100a5310519ad45b
Ellen Bennett <bennette831@gmail.com>
2023-10-26 21:13:48 -0600

    setup imports from init file, create Facility class, establish name/location/id properties w/constraints

---
+++ lib/models/facility.py
@@ -0,0 +1,36 @@
+from models.__init__ import CURSOR, CONN
+
+class Facility:
+    all = {}
+
+    def __init__(self, name, location, id=None):
+        self.id = id
+        self.name = name
+        self.location = location
+
+    def __repr__(self):
+        return f"<Facility {self.id}: {self.name} in {self.location}>"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else: 
+            raise ValueError("Must include facility name")
+        
+    @property
+    def location(self):
+        return self._location
+    
+    @location.setter
+    def location(self, location):
+        if isinstance(location, str) and len(location):
+            self._location = location
+        else:
+            raise ValueError("Must include facility location")
+        
+    
