1a84ae2eef1b5e441b3c9cdc3e847797f4810a45
Ellen Bennett <bennette831@gmail.com>
2023-10-27 13:48:03 -0600

    create facility_menu and facility_handler funcs in cli, create helper funcs to list, search, and create new facility

--- lib/cli.py
+++ lib/cli.py
@@ -1,19 +1,56 @@
 from helpers import (
-    exit_manager
+    exit_manager,
+    list_facilities,
+    search_f_name,
+    search_f_id,
+    create_facility,
+    update_facility,
+    delete_facility
 )
+
 
 def main():
     while True:
         primary_menu()
-        selection = input("> ")
-        if selection == "2":
+        p_selection = input("> ")
+        if p_selection == "2":
             exit_manager()
+        elif p_selection == "0":
+            facility_menu()
+            facility_handler()
 
 def primary_menu():
     print("Type the number of the section you need to manage: Facilities or Patients?")
     print("0 | Facilities")
     print("1 | Patients")
     print("2 | Exit Hospice Manager")
+
+def facility_menu():
+    print("How would you like to manage your facilities? Enter the corresponding number.")
+    print("0 | List all of my facilities")
+    print("1 | Search for a facility by name")
+    print("2 | Search for a facility by id number")
+    print("3 | Add a new facility to my list")
+    print("4 | Update one of my existing facilities")
+    print("5 | Remove a facility from my list")
+    print("6 | Return to main menu")
+
+def facility_handler():
+    f_selection = input("> ")
+    if f_selection == "0":
+        list_facilities()
+    elif f_selection == "1":
+        search_f_name()
+    elif f_selection == "2":
+        search_f_id()
+    elif f_selection == "3":
+        create_facility()
+    elif f_selection == "4":
+        update_facility()
+    elif f_selection == "5":
+        delete_facility()
+    elif f_selection == "6":
+        primary_menu()
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -3,3 +3,34 @@ from models.facility import Facility
 def exit_manager():
     print("Closing Hospice Manager...")
     exit() 
+
+def list_facilities():
+    facilities = Facility.get_all()
+    for f in facilities: print(f)
+
+def search_f_name():
+    name = input("Enter the facility's name: ")
+    facility = Facility.find_by_name(name)
+    print(facility) if facility else print(
+        f"{name} not found")
+
+def search_f_id():
+    id_ = input("Enter the facility's id number: ")
+    facility = Facility.find_by_id(id_)
+    print(facility) if facility else print(
+        f"Facility {id_} not found")
+
+def create_facility():
+    name = input("Enter the new facility's name: ")
+    location = input("Enter the new facility's location: ")
+    try:
+        facility = Facility.create(name, location)
+        print(f"{name} has been added to your facilities!\n{facility}")
+    except Exception as exc:
+        print("Error -- facility was not added because:", exc)
+
+def update_facility():
+    pass
+
+def delete_facility():
+    pass
