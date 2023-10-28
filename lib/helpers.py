df84a64351de43076463c02733fde02ae189369f
Ellen Bennett <bennette831@gmail.com>
2023-10-27 19:37:43 -0600

    add list_patients, search_p_id, and search_p_name helper funcs

--- lib/helpers.py
+++ lib/helpers.py
@@ -58,3 +58,47 @@ def delete_facility():
 def list_patients():
     patients = Patient.get_all()
     for p in patients: print(p)
+
+def search_p_name():
+    name = input("Enter the patient's name: ")
+    patient = Patient.find_by_name(name)
+    print(patient) if patient else print(
+        f"{name} not found")
+
+def search_p_id():
+    id_ = input("Enter the patient's id number: ")
+    patient = Patient.find_by_id(id_)
+    print(patient) if patient else print(
+        f"Patient {id_} not found")
+
+def create_facility():
+    name = input("Enter the new facility's name: ")
+    location = input("Enter the new facility's location: ")
+    try:
+        facility = Facility.create(name, location)
+        print(f"{name} has been added to your facilities!\n{facility}")
+    except Exception as exc:
+        print("Error -- facility was not added because:", exc)
+
+def update_facility():
+    id_ = input("Enter the id number of the facility you want to update: ")
+    if facility := Facility.find_by_id(id_):
+        try:
+            name = input("Enter the facility's name: ")
+            facility.name = name
+            location = input("Enter the facility's location: ")
+            facility.location = location
+            facility.update()
+            print(f"Successfully udpated: {facility}")
+        except Exception as exc:
+            print(f"Error updating this facility:", exc)
+    else: 
+        print(f"Facility {id_} not found")
+
+def delete_facility():
+    id_ = input("Enter the id number of facility you want to delete: ")
+    if facility := Facility.find_by_id(id_):
+        facility.delete()
+        print(f"Facility {id_} successfully deleted!")
+    else:
+        print(f"Facility {id_} not found")
