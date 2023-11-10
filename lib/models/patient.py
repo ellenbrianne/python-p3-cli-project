6e5a93c9be235a00af0d44a2106ec75cabb403a5
Ellen Bennett <bennette831@gmail.com>
2023-11-10 11:48:03 -0700

    remove {self.id} from __str__ in facility and patient

--- lib/models/patient.py
+++ lib/models/patient.py
@@ -12,7 +12,7 @@ class Patient:
 
     def __str__(self):
         return (
-            f"{self.id} | {self.name}, {self.diagnosis}"
+            f"{self.name}, {self.diagnosis}"
         )
     
     @property
