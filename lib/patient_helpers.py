4dcaece076f3c958fdbce0330fa85366c3eb1e9e
Ellen Bennett <bennette831@gmail.com>
2023-11-14 20:21:12 -0700

    change update_patient to use choice/index rather than patient id

--- lib/patient_helpers.py
+++ lib/patient_helpers.py
@@ -11,38 +11,42 @@ def create_patient(id_):
     diagnosis = input("Enter the new patient's diagnosis: ")
     try:
         patient = Patient.create(name, diagnosis, int(id_))
-        print(f"{name} has been added to your patients!\n{patient}")
+        print(f"{name} has been added to your patients!")
     except Exception as exc:
         print("Error -- patient was not added because:", exc)
 
 def update_patient(id_):
-    choice  = input("Enter the number of the patient you want to update: ")
+    choice  = int(input("Enter the number of the patient you want to update: "))
     patients = Patient.get_all()
-    if choice := patients[choice - 1]:
+
+    if match := patients[choice - 1]:
         try:
             name = input("Enter the patient's new name: ")
-            patient.name = name
+            match.name = name
             diagnosis = input("Enter the patient's new diagnosis: ")
-            patient.diagnosis = diagnosis
-            patient.facility_id = int(id_)
-            patient.update()
-            print(f"Successfully udpated: {patient}")
+            match.diagnosis = diagnosis
+            match.facility_id = int(id_)
+            match.update()
+            print(f"Successfully udpated: {choice} | {match.name}, {match.diagnosis}")
         except Exception as exc:
             print(f"Error updating this patient:", exc)
     else: 
-        print(f"Patient {name} not found")
+        print(f"Patient {choice} not found")
 
 def delete_patient():
-    id_ = input("Verify the number of the patient you want to delete: ")
-    if patient := Patient.find_by_id(id_):
-        patient.delete()
-        print(f"Patient {id_} successfully deleted!")
+    choice = int(input("Enter the number of the patient you want to delete: "))
+    patients = Patient.get_all()
+    if match := patients[choice - 1]:
+        match.delete()
+        print(f"Patient {choice} successfully deleted!")
     else:
-        print(f"Patient {id_} not found")
+        print(f"Patient {choice} not found")
 
+## THIS IS NO LONGER THE ID THAT IS LISTED NEXT TO A FACILITY, IT IS JUST AN INDEX...
 def match_patients():
     id_ = input("Enter the facility's number to view their patients: ")
     from models.facility import Facility
+
     if facility := Facility.find_by_id(id_):
         patients = facility.find_patients()
         print(f"{facility.name}'s PATIENTS:")
